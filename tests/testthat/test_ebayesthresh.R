9a841a974be90c2b6d030a7e1276d3f34664c48b
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-05-16 22:46:49 -0500

    Minor revision to beta.laplace test.

--- tests/testthat/test_ebayesthresh.R
+++ tests/testthat/test_ebayesthresh.R
@@ -1,9 +1,11 @@
 context("EbayesThresh")
 
 # ----------------------------------------------------------------------
-test_that("beta.laplace recovers result from EbayesThresh v1.3.2 when s=1",{
-  expect_equal(beta.laplace(c(-2,1,0,-4,5),s = 1),
-               c(0.889852029651,-0.380041716606,-0.561817771773,
-                 285.459466672351,15639.884914542939),
-               tolerance = 1e-6)
+test_that("beta.laplace recovers result from v1.3.2 package when s=1",{
+  x <- c(+0.889852029651,
+         -0.380041716606,
+         -0.561817771773,
+         +285.459466672351,
+         +15639.884914542939)
+  expect_equal(beta.laplace(c(-2,1,0,-4,5),s = 1),x,tolerance = 1e-6)
 })
