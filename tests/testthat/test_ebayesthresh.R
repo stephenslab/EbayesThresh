4c782d427391aef966edffa21685e22795ca63e6
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-05-17 10:52:13 -0500

    Small adjustment to beta.laplace test.

--- tests/testthat/test_ebayesthresh.R
+++ tests/testthat/test_ebayesthresh.R
@@ -2,10 +2,11 @@ context("EbayesThresh")
 
 # ----------------------------------------------------------------------
 test_that("beta.laplace recovers result from v1.3.2 package when s=1",{
-  x <- c(+0.889852029651,
+  x <- c(-2,1,0,-4,5)
+  y <- c(+0.889852029651,
          -0.380041716606,
          -0.561817771773,
          +285.459466672351,
          +15639.884914542939)
-  expect_equal(beta.laplace(c(-2,1,0,-4,5),s = 1),x,tolerance = 1e-6)
+  expect_equal(beta.laplace(x,s = 1),y,tolerance = 1e-6)
 })
