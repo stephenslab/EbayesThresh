8a4978d0cb9cba940e39d63b8118a6cd950b3313
stephens999 <stephens999@gmail.com>
2017-12-14 17:34:37 -0600

    make posterior median more stable

--- R/postmed.laplace.R
+++ R/postmed.laplace.R
@@ -12,9 +12,12 @@ postmed.laplace <- function(x, s = 1, w = 0.5, a = 0.5) {
 	sx <- sign(x)
 	x <- abs(x)
 	xma <- x/s - s*a
-	zz <- 1/a * (1/s*dnorm(xma)) * (1/w + beta.laplace(x, s, a))
-	zz[xma > 25] <- 1/2
-	mucor <- qnorm(pmin(zz, 1))
+	#zz <- 1/a * (1/s)*dnorm(xma) * (1/w + beta.laplace(x, s, a))
+	#zz[xma > 25] <- 1/2
+	logzz = log(1/a) + log(1/s) + dnorm(xma,log=TRUE) + log(1/w + beta.laplace(x,s,a))
+  logzz[xma>25] <- log(0.5)
+#	mucor <- qnorm(pmin(zz, 1))
+	mucor <- qnorm(pmin(logzz,0),log.p=TRUE)
 	muhat <- sx * pmax(0, xma - mucor) * s
 	return(muhat)
 }
--- tests/testthat/test_stability.R
+++ tests/testthat/test_stability.R
@@ -12,3 +12,17 @@ test_that("pmeancond and pmean2cond returns 0 for large a",{
   expect_equal(pmean2cond(1,1,biga),0,tol=1e-3)
   expect_equal(pmean2cond(-1,1,biga),0,tol=1e-3)
 })
+
+test_that("pmeancond and pmean2cond return sensible values for small a",{
+  smalla = 1e-6
+  expect_equal(pmeancond(10,1,smalla),10,tol=1e-3)
+  expect_equal(pmeancond(-10,1,smalla),-10,tol=1e-3)
+  expect_equal(pmean2cond(10,1,smalla),101,tol=1e-3)
+  expect_equal(pmean2cond(-10,1,smalla),101,tol=1e-3)
+})
+
+test_that("posterior median stable for large a",{
+  biga=1e6
+  expect_equal(postmed.laplace(3,3,0.9,100),0)
+  expect_lte(postmed.laplace(3,3,1,biga),postmed.laplace(3,3,1,10))
+})
